7df85384e2ebf99abbd811298cb1e9ba4fb5fc99
MasFuturoC <masfuturoc@gmail.com>
2021-12-19 22:07:38 -0500

    Crear dataframe

--- example.py
+++ example.py
@@ -1,1 +1,9 @@
-print ("Hola Mundo")
+import os
+import feather
+import pandas as pd
+
+#Crear dataFrame
+
+df = pd.DataFrame(columns=['nombrePDF','fechaCarga','texto','confidenciaPromedio','confidenciaStavdev'])
+
+print (df)
